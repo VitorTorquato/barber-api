e38d3e6af70e82df3c3fd2b3d4c30bfa44a68d81
Vitor Torquato de Sousa <torquatovitor68@gmail.com>
2025-03-17 16:16:31 +0100

    Detail haircut and count haircuts controller and services created

--- src/routes/haircuts.routes.ts
+++ src/routes/haircuts.routes.ts
@@ -4,6 +4,8 @@ import { CreateHairCutController } from "../controllers/haircuts/CreateHairCutCo
 import { ListHairCutController } from "../controllers/haircuts/ListHairCutController";
 import { EditHaircutController } from "../controllers/haircuts/EditHaircutController";
 import { CheckSubscriptionsController } from "../controllers/haircuts/checkSubscriptionsController";
+import { CountHaircutController } from "../controllers/haircuts/CountHaircutController";
+import { DetailHaircutController } from "../controllers/haircuts/DetailHaircutController";
 
 import{isAuthenticated} from '../middlewares/isAuthenticated';
 
@@ -13,10 +15,14 @@ const createHairCutController = new CreateHairCutController();
 const listHairCutController = new ListHairCutController();
 const editHaircutController = new EditHaircutController();
 const checkSubscriptionsController = new CheckSubscriptionsController();
+const countHaircutController = new CountHaircutController();
+const detailHaircutController = new DetailHaircutController();
 
 hairCutsRoutes.post("/createNew" , isAuthenticated , createHairCutController.handle);
 hairCutsRoutes.get("/list" , isAuthenticated , listHairCutController.handle);
 hairCutsRoutes.put("/edit" , isAuthenticated , editHaircutController.handle);
 hairCutsRoutes.get("/check" , isAuthenticated , checkSubscriptionsController.handle);
+hairCutsRoutes.get("/amount" , isAuthenticated , countHaircutController.handle);
+hairCutsRoutes.get("/haircutdetail" , isAuthenticated , detailHaircutController.handle);
 
 export {hairCutsRoutes}
